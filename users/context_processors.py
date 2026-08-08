1d4883797679b8ca8fc0e54fb009a2af81d9da54
Alan Monroy <alanmonroyzz99@gmail.com>
2026-08-07 23:57:39 -0600

    fix para urls que no esten en productivo

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,9 +1,19 @@
 from .models import MenuOpcion
+from django.urls import reverse, NoReverseMatch
 
 def menu_usuario(request):
     rol_id = request.session.get('usuario_rol')
     if rol_id is None:
         return {'menu_opciones': []}
 
-    opciones = MenuOpcion.objects.filter(rol_id=rol_id)
-    return {'menu_opciones': opciones}
+    opciones_raw = MenuOpcion.objects.filter(rol_id=rol_id)
+    opciones_validas = []
+
+    for opcion in opciones_raw:
+        try:
+            reverse(opcion.url_name)  # verifica si la URL existe
+            opciones_validas.append(opcion)
+        except NoReverseMatch:
+            pass  # ignora opciones con URLs inválidas
+
+    return {'menu_opciones': opciones_validas}
