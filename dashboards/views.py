22daebf1d08a093a39ed273a27e98db9dc523540
Alan Monroy <alanmonroyzz99@gmail.com>
2026-08-08 23:44:21 -0600

    chart

--- dashboards/views.py
+++ dashboards/views.py
@@ -11,11 +11,11 @@ def proyectos(request):
 
     proyectos = Proyectos.objects.all()
 
+    #CALCULO DE HORAS
     hoy = timezone.now().date()
     registros = []
 
     for proyecto in proyectos:
-
         # Horas acumuladas del empleado en este proyecto
         horas = ProyectosActividades.objects.filter(
             proyecto_id=proyecto.pk,
@@ -33,9 +33,65 @@ def proyectos(request):
             'dias_faltantes': dias_faltantes,
         })
 
+    #-------------ORDENAR DATOS PARA EL CHART-------------#
+    actividades = ProyectosActividades.objects.values(
+        'proyecto_id',
+        'proyecto__nombre',
+        'empleado_id',
+        'empleado__username'
+    ).annotate(
+        total_horas=Sum('horas')
+    ).order_by(
+        'proyecto_id',
+        'empleado_id'
+    )
+
+    proyectos_ids = []
+    proyectos_nombres = []
+    empleados = {}
+
+    for actividad in actividades:
+
+        proyecto_id = actividad['proyecto_id']
+        proyecto_nombre = actividad['proyecto__nombre']
+
+        empleado_id = actividad['empleado_id']
+        empleado_nombre = actividad['empleado__username']
+
+        if proyecto_id not in proyectos_ids:
+            proyectos_ids.append(proyecto_id)
+            proyectos_nombres.append(proyecto_nombre)
+
+        if empleado_id not in empleados:
+            empleados[empleado_id] = {
+                'name': empleado_nombre,
+                'data': {}
+            }
+
+        empleados[empleado_id]['data'][proyecto_id] = float(
+            actividad['total_horas'] or 0
+        )
+
+    series = []
+    for empleado in empleados.values():
+
+        data = []
+
+        for proyecto_id in proyectos_ids:
+            data.append(
+                empleado['data'].get(proyecto_id, 0)
+            )
+
+        series.append({
+            'name': empleado['name'],
+            'data': data
+        })
+    #---------------------------------------#
     context = {
         'registros':          registros,
         'reporte_breadcrumb': 'Inicio / Proyectos',
+        'chart_series': series,
+        'chart_categories': proyectos_nombres,
     }
 
     return render(request, 'dashboards/proyectos.html', context)
